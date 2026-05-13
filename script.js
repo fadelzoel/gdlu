95abcae613bb9064c2a5c4eafd029a404c65c8ad
harits1702 <95862271+harits1702@users.noreply.github.com>
2026-05-13 20:10:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
       {
         id: 1,
         name: 'WHITE GHOST',
-        price: 349.000.00,
+        price: 349,000,
         originalPrice: 599.000,
         category: 't-shirts',
         image: 'image/gallery/notFinal1.jpeg',
